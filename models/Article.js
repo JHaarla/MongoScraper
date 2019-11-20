e79a41ebbf1d2460aa0871e99542957dc4b0fad4
JHaarla <haarladesign@gmail.com>
2019-11-20 00:08:05 -0500

    creating models

--- models/Article.js
+++ models/Article.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
+// schema constructor - mongoose thing
 const Schema = mongoose.Schema;
 
+// create new object via the above Schema constructor
 const ArticleSchema = new Schema({
     title: {
         type: String,
@@ -11,8 +13,17 @@ const ArticleSchema = new Schema({
         type: String,
         required: true
     },
+    // `note` is an object that stores a Note id
+    // The ref property links the ObjectId to the Note model
+    // This allows us to populate the Article with an associated Note
     note: {
         type: Schema.Types.ObjectId,
-        ref: "Note:"      
+        ref: "Note:"
     }
 });
+
+// Creates the Article model from the above schema using mongoose's model method
+const Article = mongoose.model("Article", ArticleSchema);
+
+//Export the Article model to be used
+module.exports = Article;
