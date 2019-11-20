f1cf85fbaa6d8ecbfc1540b7b2e11e418f899621
JHaarla <haarladesign@gmail.com>
2019-11-19 23:40:36 -0500

    added more file structure

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const axios = require("axios");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+//express server setup/init
 const express = require("express");
+const app = express();
+const PORT = 5000;
+
+
+
+
+
+
+
+//start express server
+app.listen(PORT, function() {
+    console.log("Listening on port: " + PORT)
+})
